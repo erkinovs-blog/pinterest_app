27fc510108004d6c8ab3780e321b5b80b7fc0fb7
erkinovs-blog <murodjonerkinov2005@gmail.com>
2023-09-05 17:13:26 +0500

    add some files and folders

--- lib/src/common/widgets/app.dart
+++ lib/src/common/widgets/app.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:pinterest_app/src/ui/pages/controller/controller_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Pinterest App",
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => const ControllerPage(),
+        "/profile" : (context) => const ControllerPage(),
+      },
+    );
   }
 }
