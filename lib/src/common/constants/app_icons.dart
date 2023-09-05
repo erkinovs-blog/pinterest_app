eb257034ddfae55dc51d95f0017b6d625aa30a28
ibroxim4163 <ibrohimjoxongirov2@gmail.com>
2023-09-05 17:24:37 +0500

    icon paths added

---
+++ lib/src/common/constants/app_icons.dart
@@ -0,0 +1,22 @@
+abstract class AppIcons {
+  static const airbnb = "assets/icons/airbnb.svg";
+  static const arrowDown = "assets/icons/arrow_down.svg";
+  static const arrowLeft = "aassets/icons/arrow_left.svg";
+  static const arrowTop = "assets/icons/arrow_top.svg";
+  static const bell = "assets/icons/bell.svg";
+  static const boardRed = "assets/icons/board_red.svg";
+  static const board = "assets/icons/board.svg";
+  static const document1 = "assets/icons/document1.svg";
+  static const document2 = "assets/icons/document2.svg";
+  static const homeRed = "assets/icons/home_red.svg";
+  static const home = "assets/icons/home.svg";
+  static const lamp = "assets/icons/lamp.svg";
+  static const lock = "assets/icons/lock.svg";
+  static const menuMore = "assets/icons/menu_more.svg";
+  static const message = "assets/icons/message.svg";
+  static const moreHorizontal = "assets/icons/more_horizontal.svg";
+  static const pin = "assets/icons/pin.svg";
+  static const profileM = "assets/icons/profile_m.svg";
+  static const reply = "assets/icons/reply.svg";
+  static const search = "assets/icons/search.svg";
+}
